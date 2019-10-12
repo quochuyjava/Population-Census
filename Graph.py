3bb8d49941fbd9aa6bfe840e1b5452077ef7f7bb
QuocHuyNguyen <quochuyjava@users.noreply.github.com>
2019-10-12 22:11:40 +0200

    Multit-threading GUI and Smooth Data
    - GUI with Multi-threading
    - Smooth Data with Frame per Year (Frame number could be changed in Graph.py)
    - GUI need to redesign
    - Refactoring and Annotation needed

--- Graph.py
+++ Graph.py
@@ -23,73 +23,81 @@ class Graph:
         self.videoOutputFolder = videoOutputFolder
 
         self.figureList = []                                        # A List to save all rendered Figures
+        self.framePerYear = 5
 
 
     def render(self):
-        process = 0
         for x in range(self.NumberOfYears):                         # Each loop for a year
             # Variable declaration for each year
-            process = self.showLoading(process, x)                  # To show rendering process
             thisYearInt = self.dataReader.getFirstYear() + x
             thisYearObject = self.yearsDict.get(thisYearInt)
+            if x < self.NumberOfYears - 1:
+                nextYearObject = self.yearsDict.get(thisYearInt + 1)
+                topPopulationNextYearList = nextYearObject.getTopPopulationList()
             topCountriesThisYearList = thisYearObject.getTopNameList()
             topPopulationThisYearList = thisYearObject.getTopPopulationList()
 
-            # Size of the Image width x height in Inches
-            self.pic = plt.figure(figsize=(15, 10))
-            # set Style in Seabron
-            sb.set_style('dark')
-            # Draw the Graph
-            sb.barplot(
-                x=topPopulationThisYearList,
-                y=topCountriesThisYearList,
-                palette=sb.color_palette("YlOrRd_r", self.numberOfCountries))           # Color for the bars
-            # Set title
-            title = "Top " +str(self.numberOfCountries)+ " Countries with the Highest Population".upper() + '\n ' + str(thisYearInt)
-            title_obj = plt.title(title)
-            plt.setp(title_obj, color='orangered', fontsize=30)
+            for frame in range (self.framePerYear):                     # eachloop for a Frame in year
+                #change Value of each Frame
+                for index in range (len(topPopulationThisYearList)):
+                    if x < self.NumberOfYears: difference = topPopulationNextYearList[index] - topPopulationThisYearList[index]
+                    topPopulationThisYearList[index] = topPopulationThisYearList[index] + (difference * frame)/self.framePerYear
 
-            # Draw the Axes
-            plt.tick_params(axis="y",
-                            labelsize=self.getRelativeSize(14),
-                            labelrotation=15, labelcolor="g")
-            plt.tick_params(axis="x", labelsize=17, labelrotation=0, labelcolor="g",
-                            bottom=True, top=True, labeltop=True, labelbottom=True)
-            plt.xlim(right=(1.2 * self.MaxPopulationPerCountry))  # Set Length for X-Axis
-            plt.xlabel('\nPopulation in Billions', fontsize=20, color="g")  # Label for X-Axis
-            plt.ylabel('')  # Label for Y-Axis
+                # Size of the Image width x height in Inches
+                self.pic = plt.figure(figsize=(15, 10))
+                # set Style in Seabron
+                sb.set_style('dark')
+                # Draw the Graph
+                sb.barplot(
+                    x=topPopulationThisYearList,
+                    y=topCountriesThisYearList,
+                    palette=sb.color_palette("YlOrRd_r", self.numberOfCountries))           # Color for the bars
+                # Set title
+                title = "Top " +str(self.numberOfCountries)+ " Countries with the Highest Population".upper() + '\n ' + str(thisYearInt)
+                title_obj = plt.title(title)
+                plt.setp(title_obj, color='orangered', fontsize=30)
 
-            for t in range(self.numberOfCountries):                         # Each loop for a country
-                value = f'{int(topPopulationThisYearList[t]):,}'
-                plt.text(topPopulationThisYearList[t] + 50000000, t,  # gap between Bars
-                         value,
-                         color='deepskyblue', va="center", fontsize=self.getRelativeSize(17))
-                # Add rank to the graph
-                plt.text(1000000, t,
-                         str(t + 1),
-                         color='lime', va="center", fontsize=self.getRelativeSize(17))
+                # Draw the Axes
+                plt.tick_params(axis="y",
+                                labelsize=self.getRelativeSize(14),
+                                labelrotation=15, labelcolor="g")
+                plt.tick_params(axis="x", labelsize=17, labelrotation=0, labelcolor="g",
+                                bottom=True, top=True, labeltop=True, labelbottom=True)
+                plt.xlim(right=(1.2 * self.MaxPopulationPerCountry))  # Set Length for X-Axis
+                plt.xlabel('\nPopulation in Billions', fontsize=20, color="g")  # Label for X-Axis
+                plt.ylabel('')  # Label for Y-Axis
 
-            # Add World Population
-            plt.text(610000000, self.numberOfCountries/3,
-                     'TOTAL POPULATION OF THE WORLD',
-                     color='orangered', va="center", fontsize=25)
+                for t in range(self.numberOfCountries):                         # Each loop for a country
+                    value = f'{int(topPopulationThisYearList[t]):,}'
+                    plt.text(topPopulationThisYearList[t] + 50000000, t,  # gap between Bars
+                             value,
+                             color='deepskyblue', va="center", fontsize=self.getRelativeSize(17))
+                    # Add rank to the graph
+                    plt.text(1000000, t,
+                             str(t + 1),
+                             color='lime', va="center", fontsize=self.getRelativeSize(17))
 
-            # Insert world's population in the center
-            world_population = thisYearObject.getWorldPopulationThisYear()
-            world_population = self.getFormattedStr(world_population)
-            plt.text(560000000, self.numberOfCountries/2,
-                     world_population,
-                     color='orangered', va="center", fontsize=80)
+                # Add World Population
+                plt.text(610000000, self.numberOfCountries/3,
+                         'TOTAL POPULATION OF THE WORLD',
+                         color='orangered', va="center", fontsize=25)
 
-            # Signature
-            plt.text(1000000000, self.numberOfCountries * 0.9,
-                     'Võ Văn Thương - TP Hồ Chí Minh, Việt Nam, 09.2019'
-                     '\nQuoc Huy Nguyen - Hamburg, Germany, 10.2019'
-                     '\nData sources: https://data.worldbank.org',
-                     color='royalblue', va="center", fontsize=13)
+                # Insert world's population in the center
+                world_population = thisYearObject.getWorldPopulationThisYear()
+                world_population = self.getFormattedStr(world_population)
+                plt.text(560000000, self.numberOfCountries/2,
+                         world_population,
+                         color='orangered', va="center", fontsize=80)
 
-            #self.saveImages(thisYearInt, self.pic)
-            self.addFiguretoList(thisYearInt, self.pic)
+                # Signature
+                plt.text(1000000000, self.numberOfCountries * 0.9,
+                         'Võ Văn Thương - TP Hồ Chí Minh, Việt Nam, 09.2019'
+                         '\nQuoc Huy Nguyen - Hamburg, Germany, 10.2019'
+                         '\nData sources: https://data.worldbank.org',
+                         color='royalblue', va="center", fontsize=13)
+
+                #self.saveImages(thisYearInt, self.pic)
+                self.addFiguretoList(thisYearInt, self.pic)
 
     def saveImages(self, thisYear, pic):                        # Save image to png files (not in use)
         filename = 'population_' + str(thisYear) + '.png'
@@ -111,24 +119,8 @@ class Graph:
         '''
         return f'{int(population):,}'
 
-    def showLoading(self, process, x):
-        if process == 0 and x > self.NumberOfYears / 100 * 20:
-            print("Rendering 20% done")
-            return 20
-        if process == 20 and x > self.NumberOfYears / 100 * 40:
-            print("Rendering 40% done")
-            return 40
-        if process == 40 and x > self.NumberOfYears / 100 * 60:
-            print("Rendering 60% done")
-            return 60
-        if process == 60 and x > self.NumberOfYears / 100 * 80:
-            print("Rendering 80% done")
-            return 80
-        if process == 80 and x == self.NumberOfYears - 1:
-            print("Rendering 100% done")
-            return 100
-        else:
-            return process
+    def getNumberofFrames(self):
+        return self.NumberOfYears * self.framePerYear
 
     def getRelativeSize(self, size):
         '''
--- mainForThreads.py
+++ mainForThreads.py
@@ -1,3 +1,5 @@
+from threading import Timer
+
 import numpy as np
 from PyQt5 import QtWidgets as QtGui, QtWidgets
 from PyQt5.QtCore import QThread
@@ -13,6 +15,8 @@ import design2
 import pyqtgraph as pg
 import json
 import time
+FigureList = []
+NumberOfFrames = 0
 
 class getPostsThread(QThread):
     def __init__(self):
@@ -25,13 +29,52 @@ class getPostsThread(QThread):
         self.wait()
 
 
+    # Hilf Function
+    def init_years(self, dataReader):
+        import Year
+        yearsDict = {}
+        first_year = dataReader.getFirstYear()
+        worldPopulationInYears = dataReader.getWorldPopulationInYearsDict()
+        for x in range(dataReader.getCountOfYears()):
+            # print(first_year + x)
+            this_year = first_year + x
+            top_countries_in_this_year = dataReader.getTopCountries(this_year)
+            yearsDict[first_year + x] = Year.Year(this_year, top_countries_in_this_year, worldPopulationInYears)
+        return yearsDict
+    # End Hilf Function
+
     def run(self):
         """
         run in new thread
         """
-        import Start
-        Start.start()
+        # import Start          das war ok
+        # Start.start()         das war ok
+        import Graph
+        import DataReader as reader
+        import pandas as pd
+        import Year
+
+        countriesData = pd.read_excel('population -countries.xls', sep=' ', encoding='utf-8')  # Link to excel file
+        worldData = pd.read_excel('population - world.xls', sep=' ', encoding='utf-8')
+        amountOfTopCountries = 20
+        imageOutputFolder = '/Users/quochuy/Desktop/Github/Population-Census/Images'
+        videoOutputFolder = '/Users/quochuy/Desktop/Github/Population-Census/Images/Video.avi'
+        dataReader = reader.DataReader(countriesData, worldData, amountOfTopCountries)
+
+        yearsDict = self.init_years(dataReader)
+
+        graph = Graph.Graph(dataReader, yearsDict, imageOutputFolder, videoOutputFolder)
+        global FigureList
+        FigureList = graph.getFigureList()
+        global NumberOfFrames
+        NumberOfFrames = graph.getNumberofFrames()
+        graph.render()
+
+
+
+
         #self.sleep(2)
+
 
 class ThreadingTutorial(QtGui.QMainWindow, design2.Ui_MainWindow):
     """
@@ -42,22 +85,56 @@ class ThreadingTutorial(QtGui.QMainWindow, design2.Ui_MainWindow):
         super(self.__class__, self).__init__()
         self.setupUi(self)
         self.btn_start.setText('Render')
-        self.btn_start.clicked.connect(self.start_getting_top_posts)
+        self.btn_start.clicked.connect(self.doSomethingWhenButtonClicked)
 
 
-    def start_getting_top_posts(self):
+
+    def doSomethingWhenButtonClicked(self):
         '''
         When click at Start Button
         '''
         self.btn_start.setEnabled(False)
 
-        print('Start Rendering')
+        print('Nút Render dc bấm, thred mới dc mở, ở trong thread này nên chạy Graph')
         self.get_thread = getPostsThread()
         self.get_thread.start()
 
+        print('o day van chay tiep, duoi nay de gan Figure vao GUI')
 
-        self.btn_stop.setEnabled(True)
-        self.btn_stop.clicked.connect(self.get_thread.terminate)
+        self.resize(1500, 1100)
+        self._main = QtWidgets.QWidget()
+        self.setCentralWidget(self._main)
+        self.layout = QtWidgets.QVBoxLayout(self._main)
+
+        # Bat dau sua o day
+        global FigureList
+        self.frameNumber = 0  # Start rendering with frame 0
+        while len(FigureList) <= self.frameNumber:
+            print ('waiting for FigureList to init')
+            time.sleep(.200)
+        thisFigure = FigureList[self.frameNumber]
+        self.dynamic_canvas = FigureCanvas(thisFigure)
+        self.layout.addWidget(self.dynamic_canvas)
+        self._timer = self.dynamic_canvas.new_timer(100, [(self._update_canvas, (), {})])
+        self._timer.start()
+
+    def _update_canvas(self):
+        print ('Frame needed to show:' + str(self.frameNumber) + '    Frame rendered: [' + str(len(FigureList) -1) + ']')
+        while self.frameNumber >= len(FigureList) - 1:
+            print ('Frame needed to show: [' + str(self.frameNumber +1 ) + ']    Frame rendered: [' + str(len(FigureList) -1) + ']  - so we have to wait')
+            time.sleep(.200)
+        self.dynamic_canvas.setParent(None)
+        self.frameNumber = self.frameNumber + 1
+        figure = FigureList[self.frameNumber]
+        self.dynamic_canvas = FigureCanvas(figure)
+        self.layout.addWidget(self.dynamic_canvas)
+        # Delete previous Frame to save Ram
+        FigureList[self.frameNumber -1] = None
+
+        # The Graph will stop in last Frame
+        if self.frameNumber == NumberOfFrames - 1 :
+            self._timer.stop()
+            print (FigureList)
 
 
 def main():
